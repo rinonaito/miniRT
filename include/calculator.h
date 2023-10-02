ca027dd4744857a95b541943a75796577aedd310
yudai-shimoma <cdcdasdll@icloud.com>
2023-10-02 18:36:41 +0900

    #16 UPDATE: rayのy座標の変換方法修正。（光の方向の判定処理は完了していた）

--- include/calculator.h
+++ include/calculator.h
@@ -6,7 +6,7 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:52:45 by rnaito            #+#    #+#             */
-/*   Updated: 2023/09/30 14:59:48 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/10/02 18:09:35 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 # define CALCULATOR_H
 
 # include <stdlib.h>
+#include <stdbool.h>
 
 size_t	get_num_of_positive_solutions(double a, double b, double c);
 size_t	get_num_of_solutions(double a, double b, double c);
 double	get_hit_distance(double a, double b, double c);
-double	scale_to_minus_one_to_one(double value);
+double	scale_to_minus_one_to_one(double value, bool invert);
 double	scale_to_zero_to_one(double value);
 
 #endif
